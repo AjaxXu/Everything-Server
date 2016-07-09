ad735b63968ee136852976cf66472a4ddfe1830b
AjaxXu <xu_soft39211215@163.com>
2016-07-09 20:01:42 +0800

    add user router

---
+++ models/db.js
@@ -0,0 +1,6 @@
+/**
+ * Created by louis on 16/7/8.
+ */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/everything');
+
---
+++ routes/todos.js
@@ -0,0 +1,3 @@
+/**
+ * Created by louis on 16/7/8.
+ */
